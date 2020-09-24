5c816e6d8f0d8bbcd35eda696fbae904caeb0660
nodecomplete <70465448+nodecomplete@users.noreply.github.com>
2020-09-24 15:45:50 +1200

    tidied up formatting

--- DJSample/DJSampleDlg.cpp
+++ DJSample/DJSampleDlg.cpp
@@ -90,58 +90,58 @@ END_MESSAGE_MAP()
 // CDJSampleDlg message handlers
 BOOL CDJSampleDlg::OnInitDialog()
 {
-	CDialogEx::OnInitDialog();
+    CDialogEx::OnInitDialog();
 
-	// Add "About..." menu item to system menu.
+    // Add "About..." menu item to system menu.
 
-	// IDM_ABOUTBOX must be in the system command range.
-	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
-	ASSERT(IDM_ABOUTBOX < 0xF000);
+    // IDM_ABOUTBOX must be in the system command range.
+    ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+    ASSERT(IDM_ABOUTBOX < 0xF000);
 
-	CMenu* pSysMenu = GetSystemMenu(FALSE);
-	if (pSysMenu != nullptr)
-	{
-		BOOL bNameValid;
-		CString strAboutMenu;
-		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
-		ASSERT(bNameValid);
-		if (!strAboutMenu.IsEmpty())
-		{
-			pSysMenu->AppendMenu(MF_SEPARATOR);
-			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
-		}
-	}
+    CMenu* pSysMenu = GetSystemMenu(FALSE);
+    if (pSysMenu != nullptr)
+    {
+	    BOOL bNameValid;
+	    CString strAboutMenu;
+	    bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+	    ASSERT(bNameValid);
+	    if (!strAboutMenu.IsEmpty())
+	    {
+		    pSysMenu->AppendMenu(MF_SEPARATOR);
+		    pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+	    }
+    }
 
-	// Set the icon for this dialog.  The framework does this automatically
-	//  when the application's main window is not a dialog
-	SetIcon(m_hIcon, TRUE);			// Set big icon
-	SetIcon(m_hIcon, FALSE);		// Set small icon
+    // Set the icon for this dialog.  The framework does this automatically
+    //  when the application's main window is not a dialog
+    SetIcon(m_hIcon, TRUE);			// Set big icon
+    SetIcon(m_hIcon, FALSE);		// Set small icon
 
-	m_LoadFileEdit.SubclassDlgItem(IDC_EDIT1, this);
-	m_LoadFileEdit.EnableFileBrowseButton(NULL, _T("MP3 Files (*.mp3)|*.mp3|All Files (*.*)|*.*||"));
+    m_LoadFileEdit.SubclassDlgItem(IDC_EDIT1, this);
+    m_LoadFileEdit.EnableFileBrowseButton(NULL, _T("MP3 Files (*.mp3)|*.mp3|All Files (*.*)|*.*||"));
 
     m_Slider.SetRange(-100, 100);
     m_Slider.SetPos(50); 
 
-	HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
-	if (FAILED(hr))
-	{
+    HRESULT hr = CoInitializeEx(NULL, COINIT_MULTITHREADED);
+    if (FAILED(hr))
+    {
         AfxMessageBox(L"CoInitializeEx failed");
-	}
+    }
 
-	bool isDefaultDevice;
-	ERole role;
-	PickDevice(&m_pDevice, &isDefaultDevice, &role);
+    bool isDefaultDevice;
+    ERole role;
+    PickDevice(&m_pDevice, &isDefaultDevice, &role);
 
-	m_pRenderer = new CWASAPIRenderer(m_pDevice);
+    m_pRenderer = new CWASAPIRenderer(m_pDevice);
 
-	if (m_pRenderer->Initialize(m_TargetFrequency))
-	{
+    if (m_pRenderer->Initialize(m_TargetFrequency))
+    {
         if (m_pRenderer->Start())
         {
 
         }
-	} 
+    } 
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
